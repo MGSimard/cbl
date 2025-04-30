82b771909cd5c2b8384b8cd4caea285269213bef
MGSimard <mgsimard.dev@gmail.com>
2025-04-30 11:42:43 -0400

    Don't want to hack up visibility, :has(:invalid) etc to disable the button it's bad UX for accessibility, forced to use stateful form here so I can toggle :disabled on submit button

--- src/components/SumSearch.tsx
+++ src/components/SumSearch.tsx
@@ -1,23 +1,34 @@
 "use client";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SumSearch() {
+  const [region, setRegion] = useState("na");
+  const [name, setName] = useState("");
+  const [tag, setTag] = useState("");
+
   const router = useRouter();
+
+  const isFormValid = region && name.trim() && tag.trim();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
-    const region = formData.get("region");
-    const name = formData.get("name");
-    const tag = formData.get("tag");
-    router.push(`/summoner/${region}/${name}-${tag}`);
+    if (isFormValid) {
+      router.push(`/summoner/${region}/${name.trim()}-${tag.trim()}`);
+    }
   };
 
   return (
-    <form id="sum-search" onSubmit={handleSubmit}>
+    <form id="sum-search" onSubmit={handleSubmit} noValidate>
       <label htmlFor="region" id="region-label">
         <span>Region</span>
-        <select name="region" id="region" className="vis-target" required>
+        <select
+          name="region"
+          id="region"
+          className="vis-target"
+          required
+          value={region}
+          onChange={(e) => setRegion(e.target.value)}>
           <option value="na">NA</option>
           <option value="euw">EUW</option>
           <option value="kr">KR</option>
@@ -25,17 +36,34 @@ export default function SumSearch() {
       </label>
       <label htmlFor="name">
         <span>Name</span>
-        <input type="text" className="vis-target" name="name" placeholder="..." size={16} required />
+        <input
+          type="text"
+          className="vis-target"
+          name="name"
+          placeholder="..."
+          size={16}
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </label>
       <label htmlFor="tag">
         <span>Tag</span>
         <div id="tag-container" className="vis-target">
           <span>#</span>
-          <input type="text" name="tag" placeholder="..." size={5} required />
+          <input
+            type="text"
+            name="tag"
+            placeholder="..."
+            size={5}
+            required
+            value={tag}
+            onChange={(e) => setTag(e.target.value)}
+          />
         </div>
       </label>
 
-      <button type="submit" id="sum-search-submit">
+      <button type="submit" id="sum-search-submit" disabled={!isFormValid}>
         <span>SEARCH</span>
       </button>
     </form>
