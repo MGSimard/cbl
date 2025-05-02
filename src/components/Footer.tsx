5493870ffb77252bcec05bb1ed88a7c02202966e
MGSimard <mgsimard.dev@gmail.com>
2025-05-02 10:13:12 -0400

    Start reworking layout scaffolding, decided I'll be using the background on every page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,11 @@
 export function Footer() {
   return (
     <footer>
-      <div id="footer-content">
-        <p>
-          © 2025 CBL.NEXUS | Community Ban List is not endorsed by Riot Games and does not reflect the views or opinions
-          of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games and all
-          associated properties are trademarks or registered trademarks of Riot Games, Inc.
-        </p>
-      </div>
+      <p>
+        © 2025 CBL.NEXUS | Community Ban List is not endorsed by Riot Games and does not reflect the views or opinions
+        of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games and all
+        associated properties are trademarks or registered trademarks of Riot Games, Inc.
+      </p>
     </footer>
   );
 }
