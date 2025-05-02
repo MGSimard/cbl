e4d0cdce8e2fa360a0068243c999c95a69cbd197
MGSimard <mgsimard.dev@gmail.com>
2025-05-02 11:43:52 -0400

    Set up new stacking context so 404 can be behind not found without falling through body

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,10 @@ export default function NotFound() {
   return (
     <main id="not-found">
       <h1>
-        <span className="no-select">404</span>NOT FOUND
+        <span id="nf-one" className="no-select">
+          404
+        </span>
+        <span id="nf-two">NOT FOUND</span>
       </h1>
       <Link href="/" className="btn-primary-action">
         <span>RETURN HOME</span>
