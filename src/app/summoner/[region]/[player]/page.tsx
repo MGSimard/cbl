77ce11afea4d330d3c666350079f273936aa94ac
MGSimard <mgsimard.dev@gmail.com>
2025-05-02 17:20:04 -0400

    Adjustment

--- src/app/summoner/[region]/[player]/page.tsx
+++ src/app/summoner/[region]/[player]/page.tsx
@@ -1,10 +1,32 @@
 export default function PageSummoner() {
   return (
-    <main>
-      <section>
-        <h1>Summoner Page</h1>
-        Wee Woo
-      </section>
-    </main>
+    <>
+      <header>
+        <h1>Summoner Profile</h1>
+      </header>
+      <main>
+        <section>
+          <details>
+            <summary>
+              <h2>Aliases</h2>
+            </summary>
+          </details>
+          <details>
+            <summary>
+              <h2>Reports</h2>
+            </summary>
+          </details>
+          <details>
+            <summary>
+              <h2>Teammates</h2>
+            </summary>
+          </details>
+        </section>
+
+        <section>
+          <h2>Match History</h2>
+        </section>
+      </main>
+    </>
   );
 }
