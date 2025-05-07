4f015c855a56939ccaed445e27a5abd338e1df02
MGSimard <mgsimard.dev@gmail.com>
2025-05-06 23:05:30 -0400

    Error page

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+import type { Metadata } from "next";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export const metadata: Metadata = {
+  title: "Error",
+};
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main id="not-found">
+      <h1>
+        <span id="nf-one" className="no-select">
+          ERROR
+        </span>
+        <span id="nf-two">AN ERROR OCCURRED</span>
+      </h1>
+      <p id="error-message">{error.message}</p>
+      <Link href="/" className="btn-primary-action">
+        <span>RETURN HOME</span>
+      </Link>
+    </main>
+  );
+}
