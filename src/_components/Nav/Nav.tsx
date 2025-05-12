198fe7054feaf135282ab5ab8c18945f5ab67e28
MGSimard <mgsimard.dev@gmail.com>
2025-05-12 03:05:42 -0400

    Cover edge cases where nav content overflows vertically

--- src/_components/Nav/Nav.tsx
+++ src/_components/Nav/Nav.tsx
@@ -40,25 +40,27 @@ export function Nav() {
           </div>
           {isExpanded && <NavTrigger />}
         </div>
-        <NavGroup label="Aliases" count={1}>
-          <ul>
-            <li>Alias 1</li>
-          </ul>
-        </NavGroup>
-        <NavGroup label="Reports" count={2}>
-          <ul>
-            <li>Report 1</li>
-            <li>Report 2</li>
-          </ul>
-        </NavGroup>
-        <NavGroup label="Recent Players" count={3}>
-          <ul>
-            <li>Recent Player 1</li>
-            <li>Recent Player 2</li>
-            <li>Recent Player 3</li>
-          </ul>
-        </NavGroup>
-        <NavGroup label="Disabled Test" count={0}></NavGroup>
+        <div id="nav-content">
+          <NavGroup label="Aliases" count={1}>
+            <ul>
+              <li>Alias 1</li>
+            </ul>
+          </NavGroup>
+          <NavGroup label="Reports" count={2}>
+            <ul>
+              <li>Report 1</li>
+              <li>Report 2</li>
+            </ul>
+          </NavGroup>
+          <NavGroup label="Recent Players" count={3}>
+            <ul>
+              <li>Recent Player 1</li>
+              <li>Recent Player 2</li>
+              <li>Recent Player 3</li>
+            </ul>
+          </NavGroup>
+          <NavGroup label="Disabled Test" count={0}></NavGroup>
+        </div>
       </div>
     </nav>
   );
